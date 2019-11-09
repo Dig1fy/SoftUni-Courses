f322a9f8d9ed612b2eba269e8b4a844781119e04
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-11-09 23:18:38 +0200

    Task "BookShop" - finished

---
+++ C# OOP/02_Inheritance/02_BookShop/Book.cs
@@ -0,0 +1,73 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Text;
+
+    public class Book
+    {
+        private string author;
+        private string title;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            Author = author;
+            Title = title;
+            Price = price;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                this.title = value;
+            }
+        }
+        public string Author
+        {
+            get { return author; }
+
+            set
+            {
+                var authorSecondName = value.Split()[1];
+                if (char.IsDigit(authorSecondName[0]))
+                {
+                    throw new ArgumentException("Author not valid!");
+                } 
+
+                this.author = value;
+            }
+        }
+
+        public virtual decimal Price
+        {
+            get { return price; }
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var resultBuilder = new StringBuilder();
+            resultBuilder.AppendLine($"Type: { this.GetType().Name}")
+           .AppendLine($"Title: { this.Title}")
+           .AppendLine($"Author: { this.Author}")
+           .AppendLine($"Price: { this.Price:f2}");
+            string result = resultBuilder.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ C# OOP/02_Inheritance/02_BookShop/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
