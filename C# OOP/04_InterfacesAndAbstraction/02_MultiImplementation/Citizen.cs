746f7e70411e1f37b935c48b4fd2ed506e3815b2
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-11-11 19:56:16 +0200

    Added new exercise

---
+++ C# OOP/04_InterfacesAndAbstraction/02_MultiImplementation/Citizen.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
---
+++ C# OOP/04_InterfacesAndAbstraction/02_MultiImplementation/IPerson.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonInfo
+{
+    public interface IPerson
+    {
+        string Name { get; set; }
+
+        int Age { get; set; }
+    }
+}
