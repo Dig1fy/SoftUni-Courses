00d35c2eef5be49014369a1a5cee0df19c64be23
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-11-17 01:35:42 +0200

    Adding new task - ReversString

---
+++ C# Fundamentals/01_BasicSyntaxConditionalStatementsAndLoops/Exercises/MoreExercises/04_ReverseString/04_ReverseString.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+        }
+    }
+}
