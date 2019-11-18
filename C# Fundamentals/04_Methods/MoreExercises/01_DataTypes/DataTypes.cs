49c7c2f25346b87e26d7b00496d5aa10539e9d75
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-11-19 01:50:03 +0200

    Added new exercise - DataTypes

---
+++ C# Fundamentals/04_Methods/MoreExercises/01_DataTypes/DataTypes.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string action = Console.ReadLine();
+
+            switch (type)
+            {
+                case "int":
+                    int resultAsInt = GetIntResult(action); Console.WriteLine(resultAsInt);
+                    break;
+                case "real":
+                    double resultAsDouble = GetRealResult(action);
+                    Console.WriteLine($"{resultAsDouble:f2}");
+                    break;
+                case "string":
+                    GetStringResult(action);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static int GetIntResult(string action)
+        {
+            int result = int.Parse(action) * 2;
+            return result;
+        }
+
+        static double GetRealResult(string action)
+        {
+            double result = double.Parse(action) * 1.5;
+            return result;
+        }
+
+        static void GetStringResult(string action)
+        {
+            Console.WriteLine($"${action}$");
+        }
+    }
+}
