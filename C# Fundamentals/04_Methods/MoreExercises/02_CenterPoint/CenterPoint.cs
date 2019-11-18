ffc6749885ebd360dfcf54c43e1c29885ad5c24a
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-11-19 01:50:28 +0200

    Added new exercise - CenterPoint

---
+++ C# Fundamentals/04_Methods/MoreExercises/02_CenterPoint/CenterPoint.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            GetClosestPointToZero(x1, y1, x2, y2);
+        }
+        static void GetClosestPointToZero(double x1, double y1, double x2, double y2)
+        {
+
+            double firstResult = Math.Abs(x1) + Math.Abs(y1);
+            double secondResult = Math.Abs(x2) + Math.Abs(y2);
+
+            if (firstResult <= secondResult)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (firstResult > secondResult)
+            {
+                Console.WriteLine($"({x2}, {y2})"); ;
+            }
+        }
+    }
+}
