ab420eab2a478e321ab388dc5aa8fc4c9e4a01ca
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-11-23 00:09:04 +0200

    Added broken code for refactoring

---
+++ Broken AF/Core/Commands/CommandInterpreter.cs
@@ -0,0 +1,45 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core.Commands
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMANDNAME_POSTFIX = "Command";
+
+        public string Read(string inputArguments)
+        {
+            var args = inputArguments
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            var commandName = (args[0] + COMMANDNAME_POSTFIX).ToLower();
+            var commandArgs = args
+                .Skip(1)
+                .ToArray();
+
+
+            Type type = Assembly.GetCallingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name.ToLower() == commandName);
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            ICommand instanceType = Activator.CreateInstance(type) as ICommand;
+
+            if (instanceType is null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            var result = instanceType.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
+
---
+++ Broken AF/Core/Commands/HelloCommand.cs
@@ -0,0 +1,14 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            var name = args[0];
+
+            return $"Hello, {name}";
+        }
+    }
+}
