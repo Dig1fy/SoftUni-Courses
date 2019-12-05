0e79bbfbc5b58694104aed5285c598b80cdb0cfc
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-12-05 18:10:20 +0200

    Added another exercise - "Fighting Arena"

---
+++ C# OOP/07_UnitTesting/4. Arena/FightingArena.Tests/ArenaTests.cs
@@ -0,0 +1,137 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+
+public class ArenaTests
+{
+    private Arena arena;
+    private Warrior firstWarrior;
+    private Warrior secondWarrior;
+
+    [SetUp]
+    public void Setup()
+    {
+        this.firstWarrior = new Warrior("Foo", 25, 2000);
+        this.secondWarrior = new Warrior("Gosho", 25, 3000);
+
+        this.arena = new Arena();
+    }
+
+    [Test]
+    public void ConstructorShouldInitializeTheArenaWarriorsCorrectly()
+    {
+        var list = new List<Warrior>();
+
+        Assert.AreEqual(list, arena.Warriors);
+    }
+
+    [Test]
+    public void ConstructorShouldInitializeTheArenaCountCorrectly()
+    {
+        var count = arena.Count;
+
+        Assert.AreEqual(0, count);
+    }
+
+
+    [Test]
+    public void EnrollShouldIncreaseTheArenaWarriorsIfTheyHaventBeenEnrolledAlready()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        var actualCount = 1;
+
+        Assert.AreEqual(actualCount, this.arena.Count);
+        //Assert.AreEqual(this.firstWarrior, this.arena.Warriors);
+    }
+
+    [Test]
+    public void EnrollShouldIncreaseTheListOfWarriorsInTheArena()
+    {
+        this.arena.Enroll(firstWarrior);
+        var listOfWariors = new List<Warrior>();
+        listOfWariors.Add(firstWarrior);
+
+
+        Assert.AreEqual(listOfWariors, this.arena.Warriors);
+    }
+
+    [Test]
+    public void EnrollShouldIncreaseTheWarriorsOnTheArenaCorrectly()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        var listOfWarriors = new List<Warrior>();
+        listOfWarriors.Add(firstWarrior);
+
+        Assert.AreEqual(listOfWarriors, this.arena.Warriors);
+    }
+
+    [Test]
+    public void EnrollShouldThrowExceptionWhenWeRollTheSameWarriorTwice()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(firstWarrior));
+    }
+
+    [Test]
+    public void FightShouldAdjustDefendersHpAccordingToTheAttackersDamage()
+    {
+        this.arena.Enroll(firstWarrior);
+        this.arena.Enroll(secondWarrior);
+
+        var attacker = firstWarrior.Name;
+        var defender = secondWarrior.Name;
+        var expectedDefenderHp = secondWarrior.HP - firstWarrior.Damage;
+        var expectedAttackerHp = firstWarrior.HP - secondWarrior.Damage;
+
+        this.arena.Fight(attacker, defender);
+
+        Assert.AreEqual(expectedDefenderHp, secondWarrior.HP);
+        Assert.AreEqual(expectedAttackerHp, firstWarrior.HP);
+    }
+
+    [Test]
+    public void FightShouldBeAvailableOnlyForHeroesWhoAreBothEnrolledInTheArena()
+    {
+        var enemy = new Warrior("Strahil", 25, 2500);
+        this.arena.Enroll(firstWarrior);
+
+        Assert.Throws<InvalidOperationException>(() => arena.Fight(firstWarrior.Name, enemy.Name));
+    }
+
+    [Test]
+    public void FightShouldThrowExceptionIfTheDefenderIsNotEnrolledInTheArena()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        Assert.Throws<InvalidOperationException>(() => this.arena.Fight(firstWarrior.Name, "Goshooo"));
+    }
+
+    [Test]
+    public void FightShouldThrowExceptionIfTheAttackerIsNotEnrolledInTheArena()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        Assert.Throws<InvalidOperationException>(() => this.arena.Fight("Goshooo", firstWarrior.Name));
+    }
+
+    [Test]
+    public void FightShouldThrowExceptionIfTheAttackerNameIsNull()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        Assert.Throws<InvalidOperationException>(() => this.arena.Fight(null, firstWarrior.Name));
+    }
+
+    [Test]
+    public void FightShouldThrowExceptionIfTheDefenderNameIsNull()
+    {
+        this.arena.Enroll(firstWarrior);
+
+        Assert.Throws<InvalidOperationException>(() => this.arena.Fight(firstWarrior.Name, null));
+    }
+}
+
---
+++ C# OOP/07_UnitTesting/4. Arena/FightingArena.Tests/WarriorTests.cs
@@ -0,0 +1,115 @@
+using NUnit.Framework;
+using System;
+
+public class WarriorTests
+{
+    private Warrior warrior;
+
+    [SetUp]
+    public void Setup()
+    {
+        this.warrior = new Warrior("Ivo", 20, 25);
+    }
+
+    [Test]
+    public void Constructor_CheckIfParametersAreSetCorrectly()
+    {
+        Assert.AreEqual("Ivo", this.warrior.Name);
+        Assert.AreEqual(20, this.warrior.Damage);
+        Assert.AreEqual(25, this.warrior.HP);
+    }
+
+    [Test]
+    [TestCase(null, 20, 33)]
+    [TestCase(" ", 20, 33)]
+    [TestCase("", 20, 33)]
+    public void Name_ShouldThrowExceptionIfNullOrWhiteSpace(string name, int damage, int hp)
+    {
+        Warrior tempWarrior;
+
+        Assert.Throws<ArgumentException>(() => tempWarrior = new Warrior(name, damage, hp));
+    }
+
+    [Test]
+    [TestCase("Foo", 0, 33)]
+    [TestCase("Boo", -10, 33)]
+    public void Damage_ShouldThrowExceptionIfZeroOrNegativeInteger(string name, int damage, int hp)
+    {
+        Warrior tempWarrior;
+
+        Assert.Throws<ArgumentException>(() => tempWarrior = new Warrior(name, damage, hp));
+    }
+
+    [Test]
+    public void HeathCannotBeNegativeInteger()
+    {
+        Warrior tempWarrior;
+
+        Assert.Throws<ArgumentException>(() => tempWarrior = new Warrior("Foo", 20, -1));
+    }
+
+    [Test]
+    public void Attack_CannotAttackEnemiesWhenYourHpIsBelowTheMinimumHpLevel()
+    {
+        var secondWarrior = new Warrior("Boo", 20, 200);
+
+        Assert.Throws<InvalidOperationException>(() => this.warrior.Attack(secondWarrior));
+    }
+
+    [Test]
+    public void Attack_CannotAttackOtherWarriorWhichHpIslessThanTheMinimumHpLevel()
+    {
+        var firstWarrior = new Warrior("Go", 22, 222);
+        var secondWarrior = new Warrior("Boo", 20, 29);
+
+
+        Assert.Throws<InvalidOperationException>(() => firstWarrior.Attack(secondWarrior));
+    }
+
+    [Test]
+    public void Attack_CannotAttackStrongerEnemies()
+    {
+        var hero = new Warrior("ggg", 25, 100);
+        var enemy = new Warrior("Boo", 200, 2000);
+
+        Assert.Throws<InvalidOperationException>(() => hero.Attack(enemy));
+    }
+
+    [Test]
+    public void AttackShouldAdjustTheHpAccordinglyWhenAllValidationsPass()
+    {
+        var currentWarrior = new Warrior("Jose", 25, 2000);
+        var enemy = new Warrior("Boo", 12, 200);
+       var warriorHp = currentWarrior.HP - enemy.Damage;
+        //var enemyHp = enemy.HP - currentWarrior.Damage;
+
+        currentWarrior.Attack(enemy);
+
+        Assert.AreEqual(warriorHp, currentWarrior.HP);
+        //Assert.AreEqual(enemyHp, enemy.HP);
+    }
+
+    [Test]
+    public void AttackOnWarriorWithLessHpThanOurDamageShouldLeaveTheWarriorWithZeroHp()
+    {
+        var hero = new Warrior("Jan", 200, 2000);
+        var enemy = new Warrior("Bongo", 140, 190);
+
+        hero.Attack(enemy);
+
+        Assert.AreEqual(0, enemy.HP);
+
+    }
+
+    [Test]
+    public void AttackShouldReduceEnemysHealthWithTheSameValue()
+    {
+        var enemy = new Warrior("Foo", 25, 200);
+        var hero = new Warrior("Jo", 25, 200);
+
+        hero.Attack(enemy);
+
+        Assert.AreEqual(175, enemy.HP);
+        Assert.AreEqual(175, hero.HP);
+    }
+}
