e77e6660f63b363dadc75114d3cc89617b270a44
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2019-12-12 15:27:42 +0200

    Added Space Station - Structure

---
+++ C# OOP/Exams/OOP Retake Exam - 15 August 2019/1. Space Station - Structure/Models/Planets/Planet.cs
@@ -0,0 +1,32 @@
+﻿namespace SpaceStation.Models.Planets
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Planet : IPlanet
+    {
+        private string name;
+        private List<string> planetItems;
+        public Planet(string name)
+        {
+            this.Name = name;
+            this.planetItems = new List<string>();
+        }
+        
+        public ICollection<string> Items => this.planetItems;
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Invalid name!");
+                }
+
+                this.name = value;
+            }
+        }
+    }
+}
