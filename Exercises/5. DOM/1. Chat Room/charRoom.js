43ee6a465ba48b27742b5fa9e08b19d9761a805f
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2020-01-30 18:37:34 +0200

    Added DOM exercise - 'Chat Room'

---
+++ Exercises/5. DOM/1. Chat Room/charRoom.js
@@ -0,0 +1,21 @@
+function solve() {
+
+   const sendBtn = document.getElementById("send");
+   const messageContainer = document.getElementById("chat_input");
+
+   if(sendBtn === null || messageContainer === null){
+      throw new Error("Something went wrong :))");
+   }
+
+   sendBtn.addEventListener("click", sendMessage);
+
+   function sendMessage() {
+      let message = messageContainer.value
+      let newMessage = document.createElement('div');
+      newMessage.classList.add('message', 'my-message');
+      newMessage.textContent = message;
+      document.getElementById('chat_messages').appendChild(newMessage);
+      messageContainer.value = "";
+      console.log(message);
+   }
+}
