c246268fd786407c3ef24841e2c322220eb64a92
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2020-02-03 20:33:33 +0200

    Added DOM exercise - 'Shopping Cart'

---
+++ Exercises/5. DOM/7. Shopping-Cart/solution.js
@@ -0,0 +1,32 @@
+function solve() {
+
+   let textArea = document.querySelector('body > div > textarea');
+   let totalPrice = 0;
+   let listOfUniqueThings = [];
+   let allAddButtons = document.getElementsByClassName('add-product');
+   let arrayAddBtn = Array.from(allAddButtons);
+
+   for (let i = 0; i < arrayAddBtn.length; i++) {
+      arrayAddBtn[i].addEventListener('click', function one() {
+         let name = document.querySelector(`body > div > div:nth-child(${i + 2}) > div.product-details > div`).textContent;
+         let price = document.querySelector(`body > div > div:nth-child(${i + 2}) > div.product-line-price`).textContent;
+
+         if (!listOfUniqueThings.includes(name)) {
+            listOfUniqueThings.push(name);
+         }
+         let result = `Added ${name} for ${price} to the cart.\n`;
+         totalPrice += Number(price);
+         textArea.value += result;
+      });
+   }
+
+   let checkOut = document.querySelector('body > div > button');
+   checkOut.addEventListener('click', function final() {
+      let finalSting = `You bought ${listOfUniqueThings.join(', ')} for ${totalPrice.toFixed(2)}.`;
+      textArea.value += finalSting;
+
+      // disable each button after clicking 'checkout' 
+      let buttons = Array.from(document.querySelectorAll('button'));
+      buttons.forEach(button => button.disabled = true);
+   });
+}
