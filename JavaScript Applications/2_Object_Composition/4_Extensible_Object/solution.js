4e1468866218113a21e14e4e30a2cfa793acc229
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2020-03-11 10:45:33 +0200

    JS exercise - Extensible Object

---
+++ JavaScript Applications/2_Object_Composition/4_Extensible_Object/solution.js
@@ -0,0 +1,17 @@
+function solution() {
+    function extensibleObject() {
+        let obj = {
+            extend: function (template) {
+                for (let parentProp of Object.keys(template)) {
+                    if (typeof (template[parentProp]) == "function") {
+                        Object.getPrototypeOf(obj)[parentProp] = template[parentProp];
+                    } else {
+                        obj[parentProp] = template[parentProp];
+                    }
+                }
+            }
+        };
+
+        return obj;
+    }
+}
