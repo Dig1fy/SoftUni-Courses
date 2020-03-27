0c3c09061a97bca5f1e88dc3b05ee9b66f5bb902
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2020-03-27 15:21:48 +0200

    exercise on Templating - Popular Monkeys

---
+++ JavaScript Applications/7_Templating/03_Popular_Monkeys/monkeysTemplate.js
@@ -0,0 +1,24 @@
+import { monkeys } from './monkeys.js'
+
+(function () {
+    fetch('http://127.0.0.1:5500/templates/all-monkeys.hbs')
+        .then(x => x.text())
+        .then(tempHbs => {
+
+            const template = Handlebars.compile(tempHbs);
+            let context = { monkeys }
+            let monkeysHTML = template(context);
+
+            let divWrapper = document.querySelector("body > section > div");
+            divWrapper.innerHTML = monkeysHTML;
+
+            divWrapper.addEventListener('click', function (e) {
+                if (e.target.tagName === "BUTTON") {
+                    let currentStyle = e.target.parentNode.querySelector('p');
+
+                    currentStyle.style.display = currentStyle.style.display === "none" ? "block" : "none"
+                    e.target.textContent = e.target.textContent === "Info" ? "Hide Info" : "Info"
+                }
+            })
+        })
+})()
