55c6d779b443b3e50fb351f411b0889ef2dcc2d7
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2020-06-28 18:58:04 +0300

    Exercise - Villain Names

---
+++ Entity Framework Core/Exercises/01.ADO.Net/2.VillainNames/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _2.VillainNames
+{
+    class Startup
+    {
+        static void Main()
+        {
+            //First we need to create and open an connection
+            using SqlConnection connection = new SqlConnection(@"Server=. ; Database = MinionsDB; Integrated Security = true");
+            connection.Open();
+
+            //Here's the query we want to execute
+            string query = @"SELECT v.Name + ' - ' + CONVERT(NVARCHAR,COUNT(mv.MinionId)) AS Output FROM Villains AS v 
+                           JOIN MinionsVillains AS mv ON mv.VillainId = v.Id
+                           GROUP BY v.Id, v.Name
+                           HAVING COUNT(mv.MinionId) > 2
+                           ORDER BY COUNT(mv.MinionId)";
+
+            //The output is a table so we need to use SqlReader (row by row)
+            using SqlCommand command = new SqlCommand(query, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader["Output"]}");
+            }
+        }
+    }
+}
