6621f51049d4c5f212dae4a00528b9d7d6366db5
q2kPetrov <52930559+q2kPetrov@users.noreply.github.com>
2020-08-04 16:03:12 +0300

    Add ExportCustomersDto

---
+++ Entity Framework Core/Exams/C# DB Advanced Exam - 07 Apr 2019/Cinema/DataProcessor/ExportDto/XMLExportCustomersDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Cinema.DataProcessor.ExportDto
+{
+    [XmlType("Customer")]
+    public class XMLExportCustomersDto
+    {
+        [XmlAttribute("FirstName")]
+        public string FirstName { get; set; }
+
+
+        [XmlAttribute("LastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("SpentMoney")]
+        public string SpentMoney { get; set; }
+
+        [XmlElement("SpentTime")]
+        public string SpentTime { get; set; }
+    }
+}
