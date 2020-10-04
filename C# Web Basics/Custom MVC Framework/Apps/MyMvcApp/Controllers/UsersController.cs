9b4861cca388b97b5b463aca9c42e657996238b6
Q2kPetrov <ivaylozp@gmail.com>
2020-10-04 14:51:51 +0300

    Rename project

---
+++ C# Web Basics/Custom MVC Framework/Apps/MyMvcApp/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using SUS.HTTP;
+using System.Linq;
+using System.Text;
+
+namespace MyMvcApp.Controllers
+{
+    public class UsersController
+    {
+        public HttpResponse Login(HttpRequest request)
+        {
+            var responseHtml = "<h1>LOGIN !!!!!</h1>" + request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
+        public HttpResponse Register(HttpRequest request)
+        {
+            var responseHtml = "<h1>REGISTER !!!!!</h1>" + request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
+    }
+}
