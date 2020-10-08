801cae534cf8c8137cd2c8dad14d7c383800edcb
Q2kPetrov <ivaylozp@gmail.com>
2020-10-08 17:03:40 +0300

    Functionality of the view engine -writing in csthml works

--- C# Web Basics/Custom MVC Framework/SUS.MvcFramework/Controller.cs
+++ C# Web Basics/Custom MVC Framework/SUS.MvcFramework/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using SUS.HTTP;
+using SUS.MvcFramework.ViewEngine;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -7,15 +8,32 @@ namespace SUS.MvcFramework
 {
     public abstract class Controller
     {
+        private SusViewEngine viewEngine;
+
+        //Connect the view engine with each controller. This way we will be able to type ~Razor code dynamically 
+        public Controller()
+        {
+            this.viewEngine = new SusViewEngine();
+        }
+
+        //We do not support view models yet, so we will pass null to the viewContent;
+        object viewModel = null; 
+
         public HttpResponse View([CallerMemberName] string viewPath = null)
         {
             //The layout is always placed in Views/Shared/_Layout.html by convention
             var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
 
+            layout = layout.Replace("@RenderBody()", "___WE REPLACE THE VIEW CONTENT IN HERE___");
+            layout = this.viewEngine.GetHtml(layout, viewModel);
+
             //GetType will get the type of the invoked class. Then we take it's Name GetType().Name. (HomeController, CardsController, StaticFIlesController)
             var viewContent = System.IO.File.ReadAllText($"Views/{GetType().Name.Replace("Controller", string.Empty)}/{viewPath}.cshtml");
 
-            var responseHtml = layout.Replace("@RenderBody()", viewContent);
+            //This is where we connect the view engine
+            viewContent = this.viewEngine.GetHtml(viewContent, viewModel);
+
+            var responseHtml = layout.Replace("___WE REPLACE THE VIEW CONTENT IN HERE___", viewContent);
             //body length is always counted as number of bytes. 
             var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
 
