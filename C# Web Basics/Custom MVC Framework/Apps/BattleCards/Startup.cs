f6c32ae6c56af58a9b59beda97e8a31306b38084
Q2kPetrov <ivaylozp@gmail.com>
2020-10-11 16:54:33 +0300

    Automatically generate routes for static files

--- C# Web Basics/Custom MVC Framework/Apps/BattleCards/Startup.cs
+++ C# Web Basics/Custom MVC Framework/Apps/BattleCards/Startup.cs
@@ -22,12 +22,7 @@ namespace BattleCards
             routeTable.Add(new Route("/cards/add", HttpMethod.Get, new CardsController().Add));
             routeTable.Add(new Route("/cards/all", HttpMethod.Get, new CardsController().All));
             routeTable.Add(new Route("/cards/collection", HttpMethod.Get, new CardsController().Collection));
-
-            routeTable.Add(new Route("/favicon.ico", HttpMethod.Get, new StaticFilesController().Favicon));
-            routeTable.Add(new Route("/css/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().BootstrapCss));
-            routeTable.Add(new Route("/js/bootstrap.bundle.min.js", HttpMethod.Get, new StaticFilesController().BoostrapJs));
-            routeTable.Add(new Route("/css/custom.css", HttpMethod.Get, new StaticFilesController().CustomCss));
-            routeTable.Add(new Route("/js/custom.js", HttpMethod.Get, new StaticFilesController().CustomJs));
+                       
         }
 
         public void ConfigureServices()
--- C# Web Basics/Custom MVC Framework/SUS.MvcFramework/Host.cs
+++ C# Web Basics/Custom MVC Framework/SUS.MvcFramework/Host.cs
@@ -1,5 +1,7 @@
 ﻿using SUS.HTTP;
+using SUS.HTTP.Enums;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SUS.MvcFramework
@@ -9,12 +11,53 @@ namespace SUS.MvcFramework
         public static async Task CreateHostAsync(IMvcApplication application, int port = 80)
         {
             var routeTable = new List<Route>();
+
+            // This will get all files put in wwwroot (do not forget to adjust to "Copy always"). -> Example: wwwroot\\css\\bootstrap\\min.css
+            // Note that in the windows directory, the slash in opposite -> "\" instead of "/". We need to replace these and also remove "wwwroot" to get the path automatically
+            var staticFiles = Directory.GetFiles("wwwroot", "*", SearchOption.AllDirectories);
+
+            foreach (var staticFile in staticFiles)
+            {
+                // "wwwroot\\favicon.ico" => "/favicon.ico"
+                var url = staticFile.Replace("wwwroot", string.Empty).Replace("\\", "/");
+                routeTable.Add(new Route(url, HttpMethod.Get, (request) =>
+                {
+                    var fileContent = File.ReadAllBytes(staticFile);
+                    //We get the file extension of each file (.cs/.html/.jpeg etc)
+                    var fileExtension = new FileInfo(staticFile).Extension;
+
+                    //Using the new "switch" syntax
+                    var contentType = fileExtension switch 
+                    {
+                        ".txt" => "text/plain",
+                        ".js" => "text/javascript",
+                        ".css" => "text/css",
+                        ".jpg" => "image/jpg",
+                        ".jpeg" => "image/jpg",
+                        ".png" => "image/png",
+                        ".gif" => "image/gif",
+                        ".ico" => "image/vnd.microsoft.icon",
+                        ".html" => "text/html",
+                        _ => "text/plain",
+                    };
+
+                    return new HttpResponse(contentType, fileContent, HttpStatusCode.Ok);
+                }));
+            }
+
             application.ConfigureServices();
             application.Configure(routeTable);
 
+            //For debugging purposes
+            System.Console.WriteLine("All registered routes");
+            foreach (var route in routeTable)
+            {
+                System.Console.WriteLine($"{route.Method} => {route.Path}");
+            }
+
             IHttpServer server = new HttpServer(routeTable);
 
-            
+
 
 
 
