01fdc5f8b8f2b777257c7ade30c7d69f85eedcf9
Q2kPetrov <ivaylozp@gmail.com>
2021-04-06 11:42:45 +0300

    Fix: logout button works properly now

--- React JS Workshop/origami/src/App.js
+++ React JS Workshop/origami/src/App.js
@@ -2,6 +2,44 @@ import React, { useState, useEffect } from 'react'
 
 import UserContext from './Context'
 
+// // We're gonna use this upper logic to check whether the user is logged in or not (and show the appropriate pages accordingly)
+const App = (props) => {
+  const [user, setUser] = useState(props.user ? { ...props.user, loggedIn: true } : null)
+  const origamis = props.origamis || []
+
+  const logIn = (userAsObject) => {
+    setUser({
+      loggedIn: true,
+      ...userAsObject
+    })
+  }
+
+  const logOut = () => {
+    setUser({
+      loggedIn: false,
+      user: null
+    })
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        origamis,
+        user,
+        logIn,
+        logOut
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  )
+}
+
+
+export default App;
+
+
+//CLASS COMPONENT
 // class App extends React.Component{
 //   constructor(props){
 //     super(props)
@@ -47,42 +85,3 @@ import UserContext from './Context'
 // }
 
 // export default App
-
-
-
-// // We're gonna use this upper logic to check whether the user is logged in or not (and show the appropriate pages accordingly)
-const App = (props) => {
-  const [user, setUser] = useState(props.user ? { ...props.user, loggedIn: true } : null)
-  const origamis = props.origamis || []
-
-  const logIn = (userAsObject) => {
-    setUser({
-      loggedIn: true,
-      ...userAsObject
-    })
-  }
-
-  const logOut = () => {
-    setUser({
-      loggedIn: false,
-      user: null
-    })
-    this.props.history.push('/')
-  }
-
-  return (
-    <UserContext.Provider
-      value={{
-        origamis,
-        user,
-        logIn,
-        logOut
-      }}
-    >
-      {props.children}
-    </UserContext.Provider>
-  )
-}
-
-
-export default App;
